f5acd2485f1e96eb6a2ab0c1548615280dd5dd71
DianaVoicu <dianapop.pas@gmail.com>
2022-11-12 19:38:44 +0000

    Add docstring for the main function which runs the program functions

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 import sys, time
 
+
 def print_slow(string):
     """
     Print slowly the text on terminal
@@ -23,24 +24,23 @@ def start():
     print_slow('You have to guess the number within three attempts\n')
 
 
-
 def compare_num():
     """
     Generate a random number between 1-10.
     Run a while loop 3 times in which the number typed by user is
     compared with a random number.
     If the numbers are not equal if statements will print hints to the user. 
-    Another while loop runs until the value inserted by the user is valid, it has to be 
-    a number between 1-10.
+    Another while loop runs until the value inserted by the user is valid.
     """
-    guess_num = random.randint(1,10)
+    guess_num = random.randint(1, 10)
     count = 0
 
-    while count < 3 :
+    while count < 3:
         while True:
             user_num = input('Type your guess:\n')
             if validation_int(user_num):
                 break
+
         if guess_num == int(user_num):
             break   
         elif guess_num > int(user_num):
@@ -54,7 +54,7 @@ def compare_num():
         print(f'Sorry! Out of guesses! My number was {guess_num}\n')
     elif guess_num == int(user_num):
         print_slow(f'Good guess!!! The number was {guess_num}\n')
-    
+
     play_again()
   
     
@@ -64,12 +64,11 @@ def validation_int(value):
     Inside the try the value is convereted in integers.
     Raises ValueError if the value can't be converted or if the value is 
     smaller than 1 and higher than 10. 
-    
     """
     try:
         user_int = int(value)
         if user_int > 10 or user_int < 1:
-            raise ValueError (
+            raise ValueError(
                 f'Please enter a number between 1-10'
             )
     except ValueError as e:
@@ -78,27 +77,33 @@ def validation_int(value):
 
     return True
     
-def play_again ():
+
+def play_again():
     """
     Repeat the game if the user would like to continue by pressing 'y'.
-    Stope the game if the user presses the key 'n'.
+    Stop the game if the user presses the key 'n'.
     Raise a TypeError if other key than 'y' or 'n' is pressed.
     """
     choice = input('Do you wish to play again? Y/N\n')
     try:
         if choice.lower() == 'y':
             compare_num()
-        elif choice.lower()  == 'n':
+        elif choice.lower() == 'n':
+            print_slow('Bye bye! Thank you for playing with me!\n')
             exit()
         raise TypeError(
-            print(f'Not sure what you mean by {choice}...Type Y for yes or N for no')
+            print(f'Not sure what you mean by {choice}...Type Y / N')
         )
-    except TypeError as e:
+    except TypeError:
         play_again()
 
 
-
 def main():
+    """
+    Run main program functions
+    """
     start()
     compare_num()
+
+
 main()
